5f0a52c8c5155ebbc1207948a563db83ea0a1526
지승현 <101084573+201810813@users.noreply.github.com>
2025-01-08 20:21:32 +0900

    Add files via upload

---
+++ Ch2_5thWork.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Ch2_5thWork.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ch2_5thWork, "Ch2_5thWork" );
