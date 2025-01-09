b36a2cc70d91c81fd2893b9b50e01eea07c1dabc
지승현 <101084573+201810813@users.noreply.github.com>
2025-01-09 15:24:54 +0900

    Delete Ch2_5thWork.cpp

--- Ch2_5thWork.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Ch2_5thWork.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ch2_5thWork, "Ch2_5thWork" );
